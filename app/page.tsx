1adffeedc31e1b08c5523032e5a3b135d289b062
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-07-16 11:57:02 +0100

    Finisehd fetching books list data

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import HeroSection from "./_components/home/HeroSection";
-import FilterSection from "./_components/home/FilterSection";
 import BooksList from "./_components/home/BooksList";
 
 export default function Home() {
     return (
         <>
             <HeroSection />
-            <FilterSection />
             <BooksList />
         </>
     );
